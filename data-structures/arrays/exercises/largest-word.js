cd1c586edaca26f2d27385e44954bf2be6070c63
Henrique Wallace <86697313+henriquewallace@users.noreply.github.com>
2024-05-23 01:21:49 +0000

    feat: largest word exercise

---
+++ data-structures/arrays/exercises/largest-word.js
@@ -0,0 +1,25 @@
+//  input string
+// output first largest string
+
+// treat string as array
+// remove special chars with regex
+// save word in a string
+// compare current word with the saved one
+
+function LongestWord(sen) { 
+  let wordsArray = sen.split(' ');
+  let largestWord = '';
+
+  for (let i = 0; i < wordsArray.length; i++) {
+    let currentWord = wordsArray[i].replace(/[^a-zA-Z0-9 ]/g, "");
+
+    if (largestWord.length < currentWord.length) {
+      largestWord = currentWord;
+    }
+  }
+
+  return largestWord; 
+}
+
+// keep this function call here 
+console.log(LongestWord('time to gain'));
