f88be95517e09fe3f3faa2d2267e0c4f135f55a3
Henrique Wallace <86697313+henriquewallace@users.noreply.github.com>
2024-06-25 00:52:22 +0000

    feat: create my search binary tree

---
+++ data-structures/binary-trees/exercises/build-binary-search-tree.js
@@ -0,0 +1,81 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor () {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          } 
+          
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          } 
+          
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  lookup(value) {
+    if (!this.root) {
+      return null;
+    }
+
+    let currentNode = this.root;
+
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+
+    return null;
+  }
+}
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+
+  return tree;
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(tree.lookup(20))
+// console.log(JSON.stringify(traverse(tree.root)));
+
